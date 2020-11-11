612367ea932ea5762e7c76dd46f2b6f8672481f3
sophie22 <sophie22@users.noreply.github.com>
2020-11-11 12:57:24 +0000

    correct variable naming

--- resources/home/dnanexus/calc_custom_coverage.py
+++ resources/home/dnanexus/calc_custom_coverage.py
@@ -23,14 +23,18 @@ with open("inp/custom_coverage.csv", 'w') as ccc:
         hs_data = pd.read_csv(folder+"/"+file, sep='\t', header=8,
                           usecols=["coverage_or_base_quality", "high_quality_coverage_count"])
         total = sum(hs_data['high_quality_coverage_count'])
-
+        # Calculate the coverage at each depth
+        x200x = sum(hs_data.loc[hs_data['coverage_or_base_quality']>=200]['high_quality_coverage_count'])
+        x250x = sum(hs_data.loc[hs_data['coverage_or_base_quality']>=250]['high_quality_coverage_count'])
+        x300x = sum(hs_data.loc[hs_data['coverage_or_base_quality']>=300]['high_quality_coverage_count'])
+        x500x = sum(hs_data.loc[hs_data['coverage_or_base_quality']>=500]['high_quality_coverage_count'])
+        x1000x = sum(hs_data.loc[hs_data['coverage_or_base_quality']>=1000]['high_quality_coverage_count'])
         # Calculate the %coverage at each depth
-        200x = sum(hs_data.loc[hs_data['coverage_or_base_quality']>=200]['high_quality_coverage_count'])/total*100
-        250x = sum(hs_data.loc[hs_data['coverage_or_base_quality']>=250]['high_quality_coverage_count'])/total*100
-        300x = sum(hs_data.loc[hs_data['coverage_or_base_quality']>=300]['high_quality_coverage_count'])/total*100
-        500x = sum(hs_data.loc[hs_data['coverage_or_base_quality']>=500]['high_quality_coverage_count'])/total*100
-        1000x = sum(hs_data.loc[hs_data['coverage_or_base_quality']>=1000]['high_quality_coverage_count'])/total*100
-
+        p200 = x200x/total*100
+        p250 = x250x/total*100
+        p300 = x300x/total*100
+        p500 = x500x/total*100
+        p1000 = x1000x/total*100
         # Get the sample name
         fn = file.rstrip('.hsmetrics.tsv')
         if fn.endswith('.markdup') or fn.endswith('_markdup'):
@@ -39,9 +43,8 @@ with open("inp/custom_coverage.csv", 'w') as ccc:
             fn = fn[:-7]
         if fn.endswith('.duplication') or fn.endswith('_duplication') or fn.endswith('.Duplication') or fn.endswith('_Duplication'):
             fn = fn[:-12]
-
         # Write %coverage at each depth 1 sample/row
-        line = "{},{},{},{},{},{}".format(fn, 200x, 250x, 300x, 500x, 1000x)
+        line = "{},{},{},{},{},{}".format(fn, p200, p250, p300, p500, p1000)
         ccc.write(line+"\n")
 print("Done")
 
