f1e9a6cd32804d76703d1e1292365c97be98c9b0
sophie22 <rzsozso@gmail.com>
2021-10-25 14:38:21 +0100

    download metrics files specified in config.yaml

---
+++ resources/home/dnanexus/download_data.py
@@ -0,0 +1,35 @@
+# Python3.6
+
+import os
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('workflowdir', type=str)
+parser.add_argument('--multi', type=str)
+
+args = parser.parse_args()
+workflowdir = args.workflowdir
+multi = args.multi
+
+config_json = open("config.json", 'r')
+config = json.load(config_json)
+# print(type(config["single"]))
+for module in config["single"]:
+    # folder = module.split('/')[0]
+    # path = workflowdir+'/*'+folder+'*'
+    for fname in config["single"][module]:
+        # dx ls ${workflowdir}/"$folder" --folders --full | parallel -I% 'dx download $project:%/* -o ./inp/'
+        # dx find data --brief --path {} --name {} | parallel -I% 'dx download % -o ./inp/
+        os.system("dx find data --brief --path {} --name {} | parallel -I% 'dx download % -o ./inp/'".format(workflowdir, fname))
+        print("downloaded {} files".format(fname))
+
+try:
+    for module in config["multi"]:
+        # folder = module.split('/')[0]
+        path = workflowdir+'/'+multi
+        for fname in config["multi"][module]:
+            os.system("dx find data --brief --path {} --name {} | parallel -I% 'dx download % -o ./inp/'".format(path, fname))
+            print("downloaded {} files".format(fname))
+except:
+    pass
