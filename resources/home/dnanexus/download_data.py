578c9375b65b1941af1c24e33d82cb4232c14c96
sophie22 <rzsozso@gmail.com>
2021-11-03 19:16:33 +0000

    use sys.argv

--- resources/home/dnanexus/download_data.py
+++ resources/home/dnanexus/download_data.py
@@ -1,20 +1,13 @@
-#/usr/bin/python3
+# /usr/bin/python3
 
 import os
-import argparse
+import sys
 import json
-
-parser = argparse.ArgumentParser()
-parser.add_argument('workflowdir', type=str)
-parser.add_argument('--multi', type=str)
-
-args = parser.parse_args()
-workflowdir = args.workflowdir
-multi = args.multi
 
 config_json = open("config.json", 'r')
 config = json.load(config_json)
-# print(type(config["single"]))
+
+workflowdir = sys.argv[1]
 for module in config["primary"]:
     # folder = module.split('/')[0]
     # path = workflowdir+'/*'+folder+'*'
@@ -24,12 +17,13 @@ for module in config["primary"]:
         print("downloaded {} files".format(fname))
 
 try:
+    secondary_workflow = sys.argv[2]
     for module in config["secondary"]:
         # folder = module.split('/')[0]
-        path = workflowdir+'/'+multi
+        path = workflowdir+'/'+secondary_workflow
         for fname in config["secondary"][module]:
             os.system("dx find data --brief --path {} --name {} | \
                 parallel -I% 'dx download % -o ./inp/'".format(path, fname))
             print("downloaded {} files".format(fname))
 except:
-    pass
+    print("No secondary workflow provided")
