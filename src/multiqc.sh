b3ea89953d331ce2f14c61006d4926a068215a8e
Victoria Fuller <victoriaamaya1@yahoo.co.uk>
2026-02-25 12:08:54 +0000

    Add samplesheet filtering by assay code

--- src/multiqc.sh
+++ src/multiqc.sh
@@ -35,6 +35,16 @@ _parse_samplesheet_wells() {
             }
             { print $(f["Sample_ID"]), substr($(f["Sample_Well"]), 1, 1), substr($(f["Sample_Well"]), 2) }' \
         | tail -n+2 >> inputs/samplesheet_wells.tsv
+    
+    # filter file by assay code if specified in config file
+    printf "samplename\twell_row\twell_column\n" > inputs/samplesheet_wells_filtered.tsv
+
+    assay_code=$(awk -F': ' '/^assay_code:/ {print $2}' config.yaml)
+    if [[ "$assay_code" ]]; then
+        awk -v assay="$assay_code" '$1 ~ assay' inputs/samplesheet_wells.tsv >> inputs/samplesheet_wells_filtered.tsv
+    else
+        tail -n+2 inputs/samplesheet_wells.tsv >> inputs/samplesheet_wells_filtered.tsv
+    fi
 
     # turn the file into regex patterns by well row and by column for highlighting in report
     printf "well\tsamplenames\n" > inputs/samplesheet_well_samplename_patterns.tsv
